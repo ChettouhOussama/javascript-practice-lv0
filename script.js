241b186115133b20f2452ab0049909daaf59f772
OUSSAMA CHETTOUH <oussamajp8@gmail.com>
2022-10-14 17:16:32 +0100

    gg
    gg

--- script.js
+++ script.js
@@ -6,10 +6,14 @@ function fullname(){
     let lastname
 
     firstname = prompt("give me you're first name  ")
+    // the prompt is a the way we write our text in a box
     lastname = prompt("give me you're last name  ")
+
+    // the alert is the way we can show the prompt text we write
 
     alert("you're full name is " + firstname + " " + lastname )
 
 }
 
-fullname()
+// The is the execution of the function
+ 
