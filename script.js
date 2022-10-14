d40a7412890b1d7ab29832710c306384fccfb7f1
OUSSAMA CHETTOUH <oussamajp8@gmail.com>
2022-10-14 17:03:28 +0100

    gg
    gg

--- script.js
+++ script.js
@@ -1,43 +1,4 @@
 
-function divison(){
-let num1
-let num2
-let result
-
-num1 = Number(prompt("give me a the number one  "))
-num2 = Number(prompt("give me a the number one  "))
-
-result = num1 / num2
-
-alert("youre result is " + result)
-}
-
-function addision(){
-    let num1
-    let num2
-    let result
-
-    num1 = Number(prompt("give me a the number one  "))
-    num2 = Number(prompt("give me a the number one  "))
-
-    result = num1 + num2
-
-    alert("youre result is " + result)
-}
-
-function mines(){
-    let num1
-    let num2
-    let result
-
-    num1 = Number(prompt("give me a the number one  "))
-    num2 = Number(prompt("give me a the number one  "))
-
-    result = num1 - num2
-
-    alert("youre result is " + result)
-}
-
 
 function fullname(){
 
@@ -51,21 +12,4 @@ function fullname(){
 
 }
 
-
-
-
-
-
-
-
-
-firstname()
-
-
-
-divison()
-
-
-addision()
-
-mines()
+fullname()
